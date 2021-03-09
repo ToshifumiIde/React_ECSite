b3355a4eea773d4e43e799a665a026e9ad9c3700
ToshifumiIde <toshifumiide198765@gmail.com>
2021-03-09 21:13:24 +0900

    create signup function

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -24,3 +24,43 @@ export const signIn = () => {
     }
   };
 };
+
+export const SignUp = (username, email, password, confirmPassword) => {
+  return async (dispatch) => {
+    //Validation
+    if (
+      username === "" ||
+      email === "" ||
+      password === "" ||
+      confirmPassword === ""
+    ) {
+      alert("必須項目が未入力です");
+      return false;
+    }
+    if (password !== confirmPassword) {
+      alert("パスワードが一致しません。もう一度お試しください");
+      return false;
+    }
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
+        if (user) {
+          const uid = user.uid;
+          const timestamp = firebaseTimestamp.now();
+          const userInitialData = {
+            created_at: timestamp,
+            email: email,
+            role: "customer",
+            uid: uid,
+            updated_at: timestamp,
+            username: username,
+          };
+          db.collection("users")
+            .doc(uid)
+            .set(userInitialData)
+            .then(dispatch(push("/")));
+        }
+      });
+  };
+};
